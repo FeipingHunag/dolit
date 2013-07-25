9ecb150014517e0e557f3b406526fbb01041af30
feipinghuang <huang900107@163.com>
2013-07-25 15:06:50 +0800

    add parse function

--- lib/dolit.rb
+++ lib/dolit.rb
@@ -16,9 +16,60 @@ module Dolit
   attach_function :video_parse, :DLVideo_Parse, [:string, :string, :id], :status
   attach_function :video_freevideoresult, :DLVideo_FreeVideoResult, [:id], :void
 
-  def parse(url, user_agent = nil)
+  def self.parse(url, user_agent = nil)
     pResult = MemoryPointer.new :pointer
     ret = video_parse(url, user_agent, pResult)
+    return if ret != 0
+    pos = 0
+    res = {}
+    pVideoResult = pResult.get_pointer
+    res[:site_id] = pVideoResult.get_int(pos)
+    pos += 4
+    res[:time_length] = pVideoResult.get_long(pos)
+    pos += 8
+    res[:framCount] = pVideoResult.get_long(pos)
+    pos += 8
+    res[:total_size] = pVideoResult.get_long(pos)
+    pos += 8
+    res[:v_name] = pVideoResult.get_pointer(pos).read_string
+    pos += 4
+    res[:tags] = pVideoResult.get_pointer(pos).read_string
+    pos += 4
+    count = pVideoResult.get_int(pos)
+    pos += 4
+    pTypePtr = pVideoResult.get_pointer(pos)
+    pos += 4
+    if count > 0
+      p = 0
+      res[:str_typs] = []
+      0.upto(count - 1) do |i|
+        type_hash = {}
+        type_hash[:str_type] = pTypePtr.get_pointer(p).read_string
+        p += 4
+        segCount = pTypePtr.get_int(p)
+        pSegPtr = pTypePtr.get_pointer(p)
+        p += 4
 
+        if segCount > 0
+          type_hash[:files] = []
+          segPinterPos = 0
+          0.upto(segCount - 1) do |j|
+            file_hash = {}
+            file_hash[:file_size] = pSegPtr.get_long(segPinterPos)
+            segPinterPos += 8
+            file_hash[:seconds] = pSegPtr.get_int(segPinterPos)
+            segPinterPos += 4
+            file_hash[:file_no] = pSegPtr.get_int(segPinterPos)
+            segPinterPos += 4
+            file_hash[:url] = pSegPtr.get_int(segPinterPos).read_string
+            segPinterPos += 4
+            type_hash[:files][j] = file_hash
+          end
+        end
+        res[:str_typs][i] = type_hash
+      end
+    end
+    video_freevideoresult(pResult)
+    res
   end
 end
